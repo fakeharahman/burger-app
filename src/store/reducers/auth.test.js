3fcf0d682c3892461868f2c79f4473aa21e794f2
Fakeha Rahman <fakeha126@gmail.com>
2020-07-13 21:45:00 +0530

    testing

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,35 @@
+const { default: reducer } = require("./auth")
+import * as actionTypes from '../actions/actionTypes'
+
+
+describe('authjs reducer', () => {
+    it('should return initial state', () => {
+        expect(reducer(undefined, {})).toEqual({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        })
+
+    })
+    it('should store token upon login', () => {
+        expect(reducer({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        }, {
+            type: actionTypes.AUTH_SUCCESS,
+            idToken: "tokenn",
+            userId: 'userid'
+        })).toEqual({
+            token: "tokenn",
+            userId: 'userid',
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        })
+    })
+})
